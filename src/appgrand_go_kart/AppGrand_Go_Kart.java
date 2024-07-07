c15156864aba2830f16d594cc5a955a8ce1ad245
araya <araya@Yulss>
2024-07-06 18:26:34 -0600

    Proyecto

---
+++ src/appgrand_go_kart/AppGrand_Go_Kart.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appgrand_go_kart;
+
+/**
+ *
+ * @author araya
+ */
+public class AppGrand_Go_Kart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
